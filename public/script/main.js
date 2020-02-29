3bc18f66febee22aea1f936f22272ecec9131e66
Pierre-François NAVEAU <naveau.pierrefrancois@yahoo.fr>
2020-02-29 13:44:48 +0100

    Collision plateforme ok
    OK pour un bloc donné

--- public/script/main.js
+++ public/script/main.js
@@ -1,51 +1,45 @@
 window.addEventListener('load', () => {
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
-
-  //var position personnage
-        var posX = 50;
-        var posY = 590; 
-        var ballRadius = 10;
-        //valeurs déplacements
-        var dx = 7;
-        var dy = 7;
-  //contrôles
-  var rightPressed = false; //39
-  var leftPressed = false; //37
-  var upPressed = false; //38
-  var downPressed = false; //40
-  let joueur;
-  function keyDownHandler(e) {
-    if(e.keyCode == 39) {
-        rightPressed = true;
+  // var position personnage
+  var posX = 50
+  var posY = 590
+  var ballRadius = 10
+  // valeurs déplacements
+  var dx = 7
+  var dy = 7
+  // contrôles
+  var rightPressed = false // 39
+  var leftPressed = false // 37
+  var upPressed = false // 38
+  var downPressed = false // 40
+  let joueur
+  let plateFormes
+  
+  function keyDownHandler (e) {
+    if (e.keyCode == 39) {
+      rightPressed = true
+    } else if (e.keyCode == 37) {
+      leftPressed = true
+    } else if (e.keyCode == 38) {
+      upPressed = true
+    } else if (e.keyCode == 40) {
+      downPressed = true
     }
-    else if(e.keyCode == 37) {
-        leftPressed = true;
+  }
+  function keyUpHandler (e) {
+    if (e.keyCode == 39) {
+      rightPressed = false
+    } else if (e.keyCode == 37) {
+      leftPressed = false
+    } else if (e.keyCode == 38) {
+      upPressed = false
+    } else if (e.keyCode == 40) {
+      downPressed = false
     }
-    else if(e.keyCode == 38) {
-        upPressed = true;
-    }
-    else if(e.keyCode == 40) {
-        downPressed = true;
-    }
-}
-function keyUpHandler(e) {
-    if(e.keyCode == 39) {
-        rightPressed = false;
-    }
-    else if(e.keyCode == 37) {
-        leftPressed = false;
-    }
-    else if(e.keyCode == 38) {
-        upPressed = false;
-    }
-    else if(e.keyCode == 40) {
-        downPressed = false;
-    }
-}
-
-document.addEventListener("keydown",keyDownHandler);
-document.addEventListener("keyup",keyUpHandler);
+  }
+  document.addEventListener('keydown', keyDownHandler)
+  document.addEventListener('keyup', keyUpHandler)
   const terrain = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -54,130 +48,122 @@ document.addEventListener("keyup",keyUpHandler);
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ]
-  var cpt = 0;
+  var cpt = 0
   function dessinerTerrain () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    plateFormes = []
     for (let i = 0; i < 22; i++) {
       const ligne = terrain[i]
-      let plateForme = []
       let k = 0
-
       for (let j = 0; j < 15; j++) {
         if (ligne[j] === 1) {
-          plateForme[k] = new PlateForme(i, j)
+          plateFormes[k] = new PlateForme(i, j)
           k++
-          //console.log(plateForme)
         } else if (ligne[j] === 3) {
-          if(cpt==0){
-                  joueur = new Joueur(i, j)
-                  cpt++
-                }
-                else{
-                  joueur.setPosition();
-                }
-          console.log(joueur.posX)
-          console.log(joueur.posY)
+          if (cpt === 0) {
+            joueur = new Joueur(i, j)
+            cpt++
+          }
+            
         }
       }
     }
-    
+    joueur.setPosition();
   }
-  
-  // function genererTile (i, j, type) {
-  //   if (type === 1) {
-  //     const plateForme = new PlateForme(i, j)
-  //   } else if (type === 3) {
-  //     if(cpt==0){
-  //       joueur = new Joueur(i, j)
-  //       cpt++
-  //     }
-  //     else{
-  //       joueur.setPosition();
-  //     }
-  //     console.log(joueur.posX)
-  //     console.log(joueur.posY)
-  //   }
-  // }
-
   function draw () {
-    // joueur.setPosition();
-    dessinerTerrain();
-    
+    dessinerTerrain()
   }
-
-  
-
   class Joueur {
     constructor (i, j) {
-      
-
-      // ctx.beginPath()
-      // ctx.arc(j * 32 + 16, i * 32, 15, 0, Math.PI * 2, false)
-      // ctx.fillStyle = 'green'
-      // ctx.fill()
-      // ctx.closePath()
-      this.dessiner=false;
-      if(!this.dessiner){
-        console.log("if construct");
-        this.posX = j * 32 + 16
-        this.posY = i * 32
-      }
-      console.log(this.dessiner)
-      this.dessiner = true;
-      console.log(this.dessiner)
-      this.draw();
+      this.posX = j * 32
+      this.posY = i * 32
+      this.draw()
     }
-
-    draw(){
-      
+    draw () {
       ctx.beginPath()
-      ctx.arc(this.posX, this.posY, 15, 0, Math.PI * 2, false)
+      ctx.rect(this.posX, this.posY, 32, 32)
       ctx.fillStyle = 'green'
       ctx.fill()
       ctx.closePath()
-      
     }
+    setPosition () {
+      if (!this.collisionCanva()) {
+        if (rightPressed) {
+          this.posX += dx
+        } else if (leftPressed) {
+          this.posX -= dx
+        } else if (upPressed) {
+          this.posY -= dy
+        } else if (downPressed) {
+          this.posY += dy
+        }
+      }
+      if (!this.collisionPlateForme()) {
+        this.posY += dy
+      }
+      this.draw()
+    }
+    collisionCanva () {
+      if (leftPressed && this.posX - dx <= 0) {
+        this.posX = 0
+        return true
+      } else if (rightPressed && this.posX >= 480 - 32) {
+        this.posX = 480 - 32
+        return true
+      } else if (upPressed && this.posY <= 0) {
+        this.posY = 0
+        return true
+      } else if (downPressed && this.posY >= 700 - 32) {
+        this.posY = 700 - 32
+        return true
+      }
+    }
+    collisionPlateForme () {
+      //console.log(plateFormes)
+      // plateFormes.forEach(plateForme => {
+      //   console.log(this.posY);
+      //   console.log(plateForme.y);
+      //   //console.log(plateForme.x);
+      //   if(this.posY <= plateForme.y +32 + dy){
+      //     if(this.posX > plateForme.x - 32 || this.posX > plateForme.x + 32){
+      //        console.log(plateForme.x);
+      //       return true;
+      //    }
+      //   }
+      // });
 
-    setPosition(){
-      console.log("début setPosition");
-      if(rightPressed){
-        console.log("droite");
-        console.log(this.posX);
-        this.posX+=dx;
-        console.log(this.posX);
-    }else if(leftPressed){
-      this.posX-=dx;
-    }else if(upPressed){
-      this.posY-=dy;
-        
-    }else if(downPressed){
-      this.posY+=dy;
-    }
-    this.draw();
+      let collision = false;
+
+      //Test collision plateforme base
+      if(this.posY <= plateFormes[0].y -32 && this.posY >= plateFormes[0].y -32 - dy){
+        if(this.posX >= plateFormes[0].x - 32 && this.posX < plateFormes[0].x + 32){
+          collision = true;
+        }
+      }
+      console.log(collision);
+      return collision;
     }
   }
-
   class PlateForme {
     constructor (i, j) {
       this.x = j * 32 + 16
       this.y = i * 32
-
       ctx.beginPath()
       ctx.rect(j * 32, i * 32, 32, 32)
       ctx.fillStyle = '#FF0000'
@@ -185,6 +171,6 @@ document.addEventListener("keyup",keyUpHandler);
       ctx.closePath()
     }
   }
-  dessinerTerrain();
+  dessinerTerrain()
   setInterval(draw, 20)
 })
