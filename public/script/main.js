d381aeb1605efead8b9a976d5037b9c3a961593f
Pierre-François NAVEAU <naveau.pierrefrancois@yahoo.fr>
2020-03-01 13:50:36 +0100

    Update main.js
    gestion ameover socre

--- public/script/main.js
+++ public/script/main.js
@@ -1,5 +1,42 @@
-import * as homeJs from './home.js';
+
 window.addEventListener('load', () => {
+
+/*---------------------------*/
+    modalScore();
+    animationLinear()
+    chuteAlea();
+    event.preventDefault();
+
+    console.log('dom complètement chargé')
+    const sonIntro = document.querySelector("#sonIntro");
+    const sonEnnemi2 = document.querySelector('#sonEnnemi2');
+    const audioG = document.querySelector("audio");
+    let fin = document.querySelector('#fin')
+    
+    let start = document.querySelector('#start');
+    let audioLecture =  document.querySelector('#audioLecture');
+    audioLecture.onclick = ()=>{sonIntro.play();}
+    let audioPause =  document.querySelector('#audioPause');
+    audioPause.onclick = ()=>{audioG.pause();}
+    let audioStop =  document.querySelector('#audioStop');
+    audioStop.onclick = ()=>{audioG.pause();}
+    
+    // let scoreEnd = document.querySelector('#btnScore1')
+    // let move = document.querySelector('#moveCanvas')
+    let choriSon = document.querySelector('#chori');
+    let choriVertSon = document.querySelector('#choriVert');
+    // choriSon.onclick = document.querySelector('#choriSon').play();
+    choriVertSon.onclick = ()=>{sonEnnemi2.play();}
+    console.log(fin);
+    fin.onclick = gameOver;
+    
+    start.onclick = startGame;
+    // scoreEnd.onclick = modalScore;
+    // move.onclick = moveCanvas;
+
+/*-----------------------------*/
+
+
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
   
@@ -368,8 +405,9 @@ function restart(){
   function isGameOver(){
     if(vie === 0 || joueur.posY > 700){
       //appel modal
+      modalScore("gameOverhard");
       //clearInterval
-      //restart
+      //restart();
     }
   }
   class Joueur {
@@ -721,4 +759,101 @@ function restart(){
 
   dessinerTerrain()
   interval = setInterval(draw, 50)
+
+
+
+//---------------CODE HOME.JS
+
+async function gameOver(){
+  let tag = document.querySelector('#tag').value;
+  
+  let repSet = await fetch(`?controller=Save&action=saveScore&tag=${tag}&score=${score}`, {method: 'GET'});
+  // let reponse = await rep.json();
+  console.log(repSet);
+}
+function modalScore(e){
+  let element = document.querySelector('#modalScore');
+  let imgLogo = document.querySelector('#logo img');
+  let infoGame = document.querySelector('#infoGame');
+  let button = e;
+  // let audioDuLvl = document.querySelector(`#sonLvL${lvl}`);
+  element.style.display ="block";
+  infoGame.style.backgroundImage = "url('public/src/VisuelMenu1.png')";
+  if(button == 'gameOverhard'){
+      document.querySelector('#endGame').style.display="block";
+      // document.querySelector('#start').style.display="none";
+      infoGame.style.backgroundImage = "url('public/src/gameOver2.png')";
+  }else if (button == 'finPause'){
+      element.style.display ="none";
+      audioG.pause();
+  }else if (button == 'pause'){
+      audioG.pause();
+      sonIntro.play();
+  }else{
+      imgLogo.style.animation = 'dezoom 5s linear';
+  }
+}
+function moveCanvas(){
+  let testBox = document.querySelector('#box');
+  console.log(testBox);
+  let childBox = testBox.children;
+  console.log(childBox);
+  let firstChildBox = testBox.firstElementChild;// penser a vider le canvas 
+  let classFCB = firstChildBox.className;
+  if(classFCB == 'front'){
+      box.style.transform = 'translateZ(-100px) rotateY(-90deg) translateZ(-225px) translateX(-490px)';
+      childBox[0].removeAttribute('id');
+      childBox[1].setAttribute('id','canvas');
+      var newOrder = new Array(childBox[1],childBox[2],childBox[3],childBox[0]);
+      for (let index = 0; index < 4; index++) {
+          box.appendChild(newOrder[index]);
+      }
+  }else if(classFCB == 'right'){
+      box.style.transform = 'translateZ(-100px) rotateY(-180deg) translateZ(237px) translateX(-478px)';
+      childBox[0].removeAttribute('id');
+      childBox[1].setAttribute('id','canvas');
+      var newOrder = new Array(childBox[2],childBox[3],childBox[0],childBox[1]);
+      for (let index = 0; index < 4; index++) {
+          box.appendChild(newOrder[index]);
+      }
+  }else if(classFCB == 'left'){
+      box.style.transform = 'translateZ(-100px) rotateY(-270deg) translateZ(223px)';
+      childBox[0].removeAttribute('id');
+      childBox[1].setAttribute('id','canvas');
+      var newOrder = new Array(childBox[3],childBox[0],childBox[1],childBox[2]);
+      for (let index = 0; index < 4; index++) {
+          box.appendChild(newOrder[index]);
+      }
+  }else if(classFCB == 'back'){
+      gameOver;
+  }
+}
+function startGame(){
+  let element = document.querySelector('#modalScore');
+  let audio = document.querySelector("#sonLvL1");
+  element.style.display ="none";
+  audio.play();
+}
+function animationLinear(){
+  let random = Math.random()*20;
+  if(random<1){
+      random = random+1;
+  }
+  console.log(random);
+  let chori = document.querySelector('#chori');
+  let choriVert = document.querySelector('#choriVert');
+  chori.style.animation = `moveLinearLeftToRight ${random*5}s linear infinite`;
+  choriVert.style.animation = `moveLinearLeftToRight ${random*6}s linear infinite`;
+}
+function getRandomArbitrary() {
+  var random = Math.random() * (1400 - 0) + 0;
+  console.log(random);
+   return random;
+}
+function chuteAlea(){
+  let chut1 = document.querySelector('#chut1');
+  chut1.style.top = '780px';
+  chut1.style.transition = '4ms';
+}
+
 })
